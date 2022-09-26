9ca7e3bc5a05274a8af579a2555d96ab7d7a5dc4
APIBANDA <juandavidgomezruidiaz2004@gmail.com>
2022-09-26 08:38:23 -0500

    Final?

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// const whitelist = ['http://localhost:8080', 'http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000/api/v1/products', 'http://localhost:3000' ];
-// const options = {
-//   origin: (origin, callback) => {
-//     if (whitelist.includes(origin)) {
-//       callback(null, true);
-//     } else{
-//       callback(new Error('No permitido'))
-//     }
-//   }
-// }
-// app.use(cors(options));
+const whitelist = ['http://localhost:8080', 'http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000/api/v1/products', 'http://localhost:3000' ];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else{
+      callback(new Error('No permitido'))
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
